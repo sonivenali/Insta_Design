19d2a5864779deeddaaf1a97088b84e5812554dd
Venali Soni <sonivenali.vs@gmail.com>
2021-04-22 23:24:06 +0530

    Added feed

--- lib/screens/feed_widget.dart
+++ lib/screens/feed_widget.dart
@@ -10,35 +10,73 @@ class _FeedWidgetState extends State<FeedWidget> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
+      child: Column(
         children: [
-          ClipOval(
+          Row(
+            children: [
+              ClipOval(
+                child: Image.network(
+                  "http://c.files.bbci.co.uk/55CB/production/_117636912_gettyimages-1231727932.jpg",
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 14),
+                child: Text(
+                  "John",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Text(
+                  "Folllow",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700),
+                ),
+              )),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
             child: Image.network(
               "http://c.files.bbci.co.uk/55CB/production/_117636912_gettyimages-1231727932.jpg",
-              width: 60,
-              height: 60,
-              fit: BoxFit.cover,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 14),
-            child: Text(
-              "John",
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            padding: const EdgeInsets.only(top: 14, left: 8),
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Text(
+                        "421 Likes",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Positioned(
+                        right: 0,
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        )),
+                  ],
+                ),
+              ],
             ),
-          ),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              "Folllow",
-              textAlign: TextAlign.right,
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700),
-            ),
-          )),
+          )
         ],
       ),
     );
