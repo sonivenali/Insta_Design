d56c58c7f1e8eabefc932d1e82472fdea81df132
Venali Soni <sonivenali.vs@gmail.com>
2021-04-24 22:03:03 +0530

    Created Folllow Request Section

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -16,28 +16,69 @@ class _NotificationScreenState extends State<NotificationScreen> {
         centerTitle: false,
         title: Text(
           "Activity",
-          style: GoogleFonts.lora(fontSize: 24, fontWeight: FontWeight.w700),
+          style: GoogleFonts.lato(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipOval(
-                  child: Image.network(
-                    "https://wallpapercave.com/wp/wp5048074.jpg",
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Row(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 14),
+                      child: CircleAvatar(
+                          radius: 28,
+                          backgroundColor: Colors.white,
+                          backgroundImage: Image.network(
+                            "https://www.filmibeat.com/imgm/416x335x100/2021/04/janhvi-kapoor-hot-pics_161794364310.jpg",
+                            width: 60,
+                            height: 60,
+                            fit: BoxFit.cover,
+                          ).image),
+                    ),
+                    Positioned(
+                        right: 0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: Text(
+                            "1000",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ))
+                  ],
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Follow Requests",
+                        style: GoogleFonts.lato(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w700
+                        ),
+                      ),
+                      Text(
+                        "Approve or ignore requests",
+                        style: GoogleFonts.lato(color: Colors.grey,fontSize: 15),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
+
