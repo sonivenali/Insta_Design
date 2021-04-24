1de5cf73bd8ef7099546b850441ee59fa28f9d2e
Venali Soni <sonivenali.vs@gmail.com>
2021-04-24 21:29:18 +0530

    Creating Notification Screen

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class NotificationScreen extends StatefulWidget {
   @override
   _NotificationScreenState createState() => _NotificationScreenState();
@@ -7,6 +9,35 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+        title: Text(
+          "Activity",
+          style: GoogleFonts.lora(fontSize: 24, fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipOval(
+                  child: Image.network(
+                    "https://wallpapercave.com/wp/wp5048074.jpg",
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
