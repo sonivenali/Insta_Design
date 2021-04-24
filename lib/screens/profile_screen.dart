0929a8f46b37ed14c04abfa74c2203efb0366657
Venali Soni <sonivenali.vs@gmail.com>
2021-04-25 00:14:36 +0530

    Fix Bugs

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -49,130 +49,132 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 16, top: 8),
-                child: CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.white,
-                    backgroundImage: Image.network(
-                      "https://www.filmibeat.com/imgm/416x335x100/2021/04/janhvi-kapoor-hot-pics_161794364310.jpg",
-                      width: 60,
-                      height: 60,
-                      fit: BoxFit.cover,
-                    ).image),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 25),
-                child: Text(
-                  "491\nPosts",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                ),
-              ),
-              Text(
-                "211\nFollowers",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25),
-                child: Text(
-                  "192\nFollowing",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 18, top: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
               children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 8),
+                  child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.white,
+                      backgroundImage: Image.network(
+                        "https://www.filmibeat.com/imgm/416x335x100/2021/04/janhvi-kapoor-hot-pics_161794364310.jpg",
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                      ).image),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 25),
+                  child: Text(
+                    "491\nPosts",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ),
                 Text(
-                  "Veronica",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                  "211\nFollowers",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 14),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 30,
-              child: OutlineButton(
-                onPressed: () {},
-                child: Text("Edit Profile"),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 14, left: 8),
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      buildStory(
-                          'https://img.emg-services.net/HtmlPages/HtmlPage4239/switzerland-header-2.jpg',
-                          "Switzerland"),
-                      buildStory(
-                          'https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg',
-                          "Japan"),
-                      buildStory(
-                          'https://azgovernor.gov/sites/default/files/londs_0.jpg',
-                          "London"),
-                      buildStory(
-                          'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900',
-                          "Paris"),
-                      buildStory(
-                          'https://static.toiimg.com/photo/77757963.cms',
-                          "New York"),
-                      buildStory(
-                          'https://static.toiimg.com/photo/75404966.cms',
-                          "Italy"),
-                      buildStory(
-                          'https://thecommonwealth.org/sites/default/files/styles/press_release_large/public/images/hero/taj-mahal-620.jpg?itok=PKSpaEMm',
-                          "India"),
-                      buildStory(
-                          'https://www.studying-in-germany.org/wp-content/uploads/2018/07/German-Culture-and-Traditions.jpg',
-                          "Germany"),
-                      buildStory(
-                          'https://media.worldnomads.com/travel-safety/algeria/terrorism-algeria-lead.jpg',
-                          "Algeria"),
-                      buildStory(
-                          'https://topadventure.com/__export/1603739147070/sites/laverdad/img/2020/10/26/las_mejores_ciudades_para_hacer_turismo_de_negocios_en_mxxicox.jpg_1183029055.jpg',
-                          "Mexico"),
-                    ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "192\nFollowing",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
                   ),
                 ),
               ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16,left: 30,right: 30),
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.widgets_outlined,color: Colors.grey,),
-                Icon(Icons.live_tv_outlined,color: Colors.grey,),
-                Icon(Icons.person_pin_outlined,color: Colors.grey,)
-              ],
+            Padding(
+              padding: const EdgeInsets.only(left: 18, top: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Veronica",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                  ),
+                ],
+              ),
             ),
-          ),
-          GridView.count(
-            crossAxisCount: 3,
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            padding: EdgeInsets.only(top: 12),
-            children: imageList.map((link) => buildImage(link)).toList(),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 14),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 30,
+                child: OutlineButton(
+                  onPressed: () {},
+                  child: Text("Edit Profile"),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 14, left: 8),
+              child: Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        buildStory(
+                            'https://img.emg-services.net/HtmlPages/HtmlPage4239/switzerland-header-2.jpg',
+                            "Switzerland"),
+                        buildStory(
+                            'https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg',
+                            "Japan"),
+                        buildStory(
+                            'https://azgovernor.gov/sites/default/files/londs_0.jpg',
+                            "London"),
+                        buildStory(
+                            'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900',
+                            "Paris"),
+                        buildStory(
+                            'https://static.toiimg.com/photo/77757963.cms',
+                            "New York"),
+                        buildStory(
+                            'https://static.toiimg.com/photo/75404966.cms',
+                            "Italy"),
+                        buildStory(
+                            'https://thecommonwealth.org/sites/default/files/styles/press_release_large/public/images/hero/taj-mahal-620.jpg?itok=PKSpaEMm',
+                            "India"),
+                        buildStory(
+                            'https://www.studying-in-germany.org/wp-content/uploads/2018/07/German-Culture-and-Traditions.jpg',
+                            "Germany"),
+                        buildStory(
+                            'https://media.worldnomads.com/travel-safety/algeria/terrorism-algeria-lead.jpg',
+                            "Algeria"),
+                        buildStory(
+                            'https://topadventure.com/__export/1603739147070/sites/laverdad/img/2020/10/26/las_mejores_ciudades_para_hacer_turismo_de_negocios_en_mxxicox.jpg_1183029055.jpg',
+                            "Mexico"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16,left: 30,right: 30),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.widgets_outlined,color: Colors.grey,),
+                  Icon(Icons.live_tv_outlined,color: Colors.grey,),
+                  Icon(Icons.person_pin_outlined,color: Colors.grey,)
+                ],
+              ),
+            ),
+            GridView.count(
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.only(top: 12),
+              children: imageList.map((link) => buildImage(link)).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
